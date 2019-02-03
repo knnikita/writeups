2830f659c8c34a60455def526f4504be4364c29f
root <root@nikita.localdomain>
2019-02-03 23:33:53 +0300

    added solve

---
+++ nullcon19/Dr_Owl/solve.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import os
+import string
+
+def run(msg):
+    os.system("/root/Desktop/pin3.7/pin -t /root/Desktop/pin3.7/source/tools/ManualExamples/obj-intel64/inscount0.so -- /mnt/hgfs/D/current/dr-owl \"" + "hackim19{" + msg + "\"")
+    #print msg
+    return int(read("inscount.out").split(" ")[1])
+
+
+def read(fname):
+    with open(fname) as f:
+        return f.read()
+
+# charset = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_{}123456780'"
+# charset = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+charset = string.letters + string.digits + " {}=_-!+"
+l = []
+flag = ""
+counter = 0
+
+while len(flag) != 42:
+    l = []
+    for a in charset:
+        count = run((flag + a).ljust(42, "*"))
+        l.append((count, a))
+    l = sorted(l, reverse=True)
+    best = l[0][1] # choose the letter with the biggest number of executed instructions
+    flag = flag + best
+    print flag
